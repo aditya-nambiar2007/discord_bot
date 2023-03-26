9a3cdc17847b0247f4551024b11e22a97dc7df55
aditya-nambiar2007 <99115432+aditya-nambiar2007@users.noreply.github.com>
2023-03-26 17:12:14 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,20 +1,26 @@
 import discord
-from discord import app_commands as command
+from discord import app_commands
+import json
 
 
-points={}
+points=json.load( open("data.json","r") ) 
+
 
 client = discord.Client(intents=discord.Intents.default())
 
-cmd=command.CommandTree(client)
+cmd=app_commands.CommandTree(client)
 
 @cmd.command(name = "register", description = "REGISTER FOR 100 POINTS",guild=discord.Object(id=984131715721224203))
-async def first_command(interact):
-    points[interact.author]=points.get(interact.author) or 100
-    await interact.response.send_message(f'You Have {points.get(interact.author)} points')
+async def first_command(interaction):
+    points[interaction.author]=points.get(interaction.author) or 100
+    await interaction.response.send_message(f'You Have {points.get(interaction.author)} points')
 
 @client.event
 async def on_message(message):
     print(message.guild.id)
 
 client.run('MTA4OTUwNTE0NDY5NTE2MDgzMg.GZzkiP.RAtqsHtChmDzxUei31rGmLTfxs1eEuwk2MqfOk')
+
+while 1==1:
+    f=open("data.json","w")
+    f.write(json.dumps(points))
